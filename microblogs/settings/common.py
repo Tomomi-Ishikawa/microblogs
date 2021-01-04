825789b097bb615e0ab0b1d043399c0bc3de9101
tomomi ishikawa <>
2021-01-04 20:12:42 +0900

    static

--- microblogs/settings/common.py
+++ microblogs/settings/common.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -117,5 +116,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "assets"),
 )
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, "static”)) 
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, "static"))
 LOGIN_REDIRECT_URL = '/'
